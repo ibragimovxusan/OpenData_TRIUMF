64b54f89d931d180c69df514a25fba6a972eb5e6
ibragimovxusan <lazy.devsloper@gmail.com>
2024-01-28 14:25:46 +0500

    api project

--- api/mobile/urls/v1.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('mobile/', include(('api.mobile.organization.urls', 'organization'))),
-    path('mobile/', include(('api.mobile.account.urls', 'account'))),
-    path('mobile/', include(('api.mobile.letter.urls', 'letter'))),
-
-]
